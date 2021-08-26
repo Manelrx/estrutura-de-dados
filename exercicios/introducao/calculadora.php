b1cf7eb07b3f5191978ac4d3ad6e31adc813dc8a
Emanuel Manelrx <83252512+Manelrx@users.noreply.github.com>
2021-08-26 11:41:52 -0300

    atividade finalizada

--- exercicios/introducao/calculadora.php
+++ exercicios/introducao/calculadora.php
@@ -2,36 +2,41 @@
 
 //1) Elabore uma calculadora onde o usuário informará dois números e uma operação (+, -, *, /) e retorne o resultado da operação.
 $parar = false;
+define("padrao","\033[0m");
+define("vermelho","\033[31m");
+define("verde","\033[33m");
+define("azul","\033[34m");
+
 do{
     $validaNumeros = false;
-     $opcao = readline('1-somar \n 2- Subtrair \n 3-Multiplicar \n 4-Dividir \n 0-Sair');
+    $opcao = readline(verde. '1-somar \n 2- Subtrair \n 3-Multiplicar \n 4-Dividir \n 0-Sair: ');
     while($validaNumeros == false){
-        $primeiroValor = readline('Informe o primeiro valor: ');
-        $segundoValor = readline('Informe o segundo valor');
+        $primeiroValor = readline(vermelho. 'Informe o primeiro valor: ');
+        $segundoValor = readline(azul. 'Informe o segundo valor: ');
         if($opcao == 4 and ($primeiroValor == 0 or $segundoValor == 0)){
-            echo 'não é possivel subtrair por 0';
-        }else{}
-    }
-    
-    echo ('+ soma, - substração, * multiplicação e / divisão');
-
-    switch($opcao){
-        case 1:
-            echo "$primeiroValor \+ $segundoValor = $primeiroValor + $segundoValor";
-        case 2:
-            echo "$primeiroValor \- $segundoValor = $primeiroValor - $segundoValor";
-        case 3: 
-            echo "$primeiroValor vezes $segundoValor = $primeiroValor * $segundoValor";
-        case 4: 
-            echo "$primeiroValor dividido $segundoValor = $primeiroValor / $segundoValor";
-    }
-
-
+            echo 'não é possivel subtrair por 0 \n ';
+        }else{
+          $opcao = true;
+          switch($opcao){
+            case 0: 
+                echo "Até a proxima";
+                $parar = false;
+                break;
+            case 1:
+                echo padrao. "$primeiroValor \+ $segundoValor = $primeiroValor + $segundoValor\n";
+                break;
+            case 2:
+                echo azul. "$primeiroValor \- $segundoValor = $primeiroValor - $segundoValor \n";
+                break;
+            case 3: 
+                echo vermelho. "$primeiroValor vezes $segundoValor = $primeiroValor * $segundoValor \n";
+                break;
+            case 4: 
+                echo verde. "$primeiroValor dividido $segundoValor = $primeiroValor / $segundoValor \n";
+                break;
+            }       
+        }
+    }   
 }while($parar == false);
-
-
-
-
-
 
 ?>
