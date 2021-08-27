87e0aad4e292c255b3497cdfdcaa0aafde240159
Emanuel Manelrx <83252512+Manelrx@users.noreply.github.com>
2021-08-27 10:40:25 -0300

    atualização

--- exercicios/introducao/calculadora.php
+++ exercicios/introducao/calculadora.php
@@ -4,17 +4,18 @@
 $parar = false;
 define("padrao","\033[0m");
 define("vermelho","\033[31m");
-define("verde","\033[33m");
+define("verde","\033[32m");
 define("azul","\033[34m");
 
 do{
     $validaNumeros = false;
-    $opcao = readline(verde. '1-somar \n 2- Subtrair \n 3-Multiplicar \n 4-Dividir \n 0-Sair \n Qual operação deseja realizar: ');
+    echo verde. "1-somar \n 2- Subtrair \n 3-Multiplicar \n 4-Dividir \n 0-Sair \n";
+    $opcao = readline("Qual operação deseja realizar: ");
     while($validaNumeros == false){
-        $primeiroValor = readline(vermelho. 'Informe o primeiro valor: ');
-        $segundoValor = readline(azul. 'Informe o segundo valor: ');
+        $primeiroValor = readline(vermelho. "Informe o primeiro valor: ");
+        $segundoValor = readline(azul. "Informe o segundo valor: ");
         if($opcao == 4 and ($primeiroValor == 0 or $segundoValor == 0)){
-            echo 'não é possivel subtrair por 0 \n ';
+            echo "não é possivel subtrair por 0 \n ";
         }else{
           $validaNumeros = true;
           switch($opcao){
@@ -23,21 +24,19 @@ do{
                 $parar = false;
                 break;
             case 1:
-                $resultado = $primeiroValor + $segundoValor;
-                echo padrao. "$primeiroValor \+ $segundoValor = $resultado\n";
+                echo padrao. "$primeiroValor + $segundoValor = " . $primeiroValor + $segundoValor . "\n";
                 break;
-            case 2:
-                $resultado = $primeiroValor - $segundoValor;
-                echo azul. "$primeiroValor \- $segundoValor = $resultado \n";
+            case 2: 
+                echo azul. "$primeiroValor - $segundoValor = " . $primeiroValor - $segundoValor . "\n";
                 break;
             case 3: 
-                $resultado = $primeiroValor * $segundoValor;
-                echo vermelho. "$primeiroValor vezes $segundoValor = $resultado \n";
+                echo vermelho. "$primeiroValor vezes $segundoValor = " . $primeiroValor * $segundoValor . "\n";
                 break;
             case 4: 
-                $resultado = $primeiroValor / $segundoValor;
-                echo verde. "$primeiroValor dividido $segundoValor = $resultado \n";
+                echo verde. "$primeiroValor dividido $segundoValor = " . $primeiroValor / $segundoValor . "\n";
                 break;
+            default:
+                echo "Essa opção não existe, favor informe novamente \n";
             }       
         }
     }   
